bfaef80b60a461c35135f51787e7a8d649990d9d
hb1003 <hb1003@hb1003>
2019-03-05 17:59:40 +0900

    2019-03-05 "18:00"

--- bit_etland/src/main/java/com/bit_etland/web/controller/HomeController.java
+++ bit_etland/src/main/java/com/bit_etland/web/controller/HomeController.java
@@ -3,17 +3,32 @@ package com.bit_etland.web.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes({"ctx", "css", "js", "img", "time"})
 @Controller
 public class HomeController {
+	static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String date(Model model){
-		model.addAttribute("serverTime", new SimpleDateFormat("yy-MM-dd").format(new Date()));
-		return "home";
+	public String date(HttpSession session, HttpServletRequest request){
+		
+		logger.info("\n --------- Welcome {} !! ----------","Home");
+		
+		String ctx = request.getContextPath();
+		session.setAttribute("ctx", ctx);
+		session.setAttribute("css", ctx+"/resources/css/");
+		session.setAttribute("js", ctx+"/resources/js/");
+		session.setAttribute("img", ctx+"/resources/img/");
+		session.setAttribute("time", new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm:ss").format(new Date()));
+		return "home/main";
 	}
 }
